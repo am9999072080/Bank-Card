730934fcce19e5162f95559ef63f486bf50652c7
am999 <am9999072080@gmail.com>
2024-04-26 06:14:00 +0300

    commit-b

--- src/main/java/com/mlan/BankCard/debit/DebAccumulation.java
+++ src/main/java/com/mlan/BankCard/debit/DebAccumulation.java
@@ -1,5 +1,59 @@
 package com.mlan.BankCard.debit;
 
-public class DebAccumulation {
+import java.util.Objects;
 
+public class DebAccumulation extends DebBigBonus {
+    private final double percentAccumulation = 0.005;
+    private double accumulation;
+
+    public DebAccumulation() {
+    }
+
+    public DebAccumulation(double balance) {
+        super(balance);
+    }
+
+
+    @Override
+    public void addBalance(double amount) throws RuntimeException {
+        super.addBalance(amount);
+        double d = amount * percentAccumulation;
+        accumulation = accumulation + d;
+        System.out.println("Накопление от пополнения: " + d);
+        System.out.println("Баланс накопления: " + accumulation);
+
+
+    }
+
+    @Override
+    public String checkAllBalance() {
+
+        return super.checkAllBalance() + " ACCUMULATION: " + accumulation;
+    }
+
+    public double getPercentAccumulation() {
+        return percentAccumulation;
+    }
+
+    public double getAccumulation() {
+        return accumulation;
+    }
+
+    public void setAccumulation(double accumulation) {
+        this.accumulation = accumulation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DebAccumulation that = (DebAccumulation) o;
+        return Double.compare(percentAccumulation, that.percentAccumulation) == 0 && Double.compare(accumulation, that.accumulation) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), percentAccumulation, accumulation);
+    }
 }
--- src/main/java/com/mlan/BankCard/debit/DebCash.java
+++ src/main/java/com/mlan/BankCard/debit/DebCash.java
@@ -1,5 +1,62 @@
 package com.mlan.BankCard.debit;
 
-public class DebCash {
+import java.util.Objects;
 
+public class DebCash extends DebitCard {
+    private final double bonusForBuy = 0.01;
+    private double bonus;
+
+
+    public DebCash(double balance) {
+        super(balance);
+    }
+
+    public DebCash() {
+    }
+
+    @Override
+    public boolean pay(double amount) throws RuntimeException {
+        double d;
+        if (super.pay(amount)) {
+            if (getBalance() >= amount) {
+                d = amount * bonusForBuy;
+                bonus += d;
+                setBalance(getBalance() - amount);
+                System.out.println("Покупка, DEBIT: " + -amount);
+
+                System.out.println("Кешбэк: " + d);
+                System.out.println("Бонусный баланс: " + bonus);
+            } else {
+                throw new RuntimeException("Недостаточно средств!");
+            }
+        }
+        return super.pay(amount);
+    }
+
+
+    public double getBonusForBuy() {
+        return bonusForBuy;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DebCash debCash = (DebCash) o;
+        return Double.compare(bonusForBuy, debCash.bonusForBuy) == 0 && Double.compare(bonus, debCash.bonus) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), bonusForBuy, bonus);
+    }
 }
